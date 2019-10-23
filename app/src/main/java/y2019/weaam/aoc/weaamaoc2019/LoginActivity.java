74f37c5d8f2d4c85dbc9079c4a06d955c4461087
weaamsh13 <shehadeweaam@gmail.com>
2019-10-23 11:12:08 +0300

    initial

--- app/src/main/java/y2019/weaam/aoc/weaamaoc2019/LoginActivity.java
+++ app/src/main/java/y2019/weaam/aoc/weaamaoc2019/LoginActivity.java
@@ -1,13 +1,21 @@
 package y2019.weaam.aoc.weaamaoc2019;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.JobIntentService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.widget.Toast.makeText;
 
@@ -15,11 +23,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     //1, properties definition
     EditText editTextEmail , editTextPassword;
     Button buttonLogIn, buttonSignUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         //2.initialize properties
         editTextEmail=findViewById(R.id.editTextEmail);
@@ -32,15 +43,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void signIn (String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("FirebaseAuth", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                            i.putExtra("email" , editTextEmail.getText().toString());
+                            i.putExtra("password" , editTextPassword.getText().toString());
+                            startActivity(i);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("FirebaseAuth", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+
+    @Override
     public void onClick(View v) {
         if(v==buttonLogIn){
             if(editTextEmail.getText().toString().equals("")|| editTextPassword.getText().toString().equals("")){
                 Toast.makeText(this,"Empty Password or Email",Toast.LENGTH_LONG).show();
             }
-            else{ Intent i=new Intent(this,MainActivity.class);
-                i.putExtra("email" , editTextEmail.getText().toString());
-                i.putExtra("password" , editTextPassword.getText().toString());
-                startActivity(i);}
+            else{ signIn(editTextEmail.getText().toString(),editTextPassword.getText().toString() );}
 
         }
         else{
--- app/src/main/java/y2019/weaam/aoc/weaamaoc2019/SignUpActivity.java
+++ app/src/main/java/y2019/weaam/aoc/weaamaoc2019/SignUpActivity.java
@@ -7,13 +7,22 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.io.FileNotFoundException;
 
@@ -29,12 +38,15 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     Button buttonCamera;
     Button buttonGallery;
     Bitmap bitmap;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         listItems = getResources().getStringArray(R.array.club_list);
 
@@ -71,11 +83,47 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
-    public void onClick(View v) {
-        if(v==buttonRegister) {
-            Intent i=new Intent(this,LoginActivity.class);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
 
-        startActivity(i);
+    public void signUp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("FirebaseAuth", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i=new Intent(SignUpActivity.this,LoginActivity.class);
+                            startActivity(i);
+
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("FirebaseAuth", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    public void onClick(View v) {
+
+
+        if(v==buttonRegister) {
+            signUp(editTextEmail.getText().toString(),editTextPassword.getText().toString() );
     }
         if(v == buttonCamera) {
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
