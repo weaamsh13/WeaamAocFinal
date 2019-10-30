3dd058c346ed0b84aa3c3ef562bc6fb6fc8bf615
weaamsh13 <shehadeweaam@gmail.com>
2019-10-30 11:09:05 +0200

    z

--- app/src/main/java/y2019/weaam/aoc/weaamaoc2019/LiverpoolInfoActivity.java
+++ app/src/main/java/y2019/weaam/aoc/weaamaoc2019/LiverpoolInfoActivity.java
@@ -5,20 +5,29 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class LiverpoolInfoActivity extends AppCompatActivity {
+public class LiverpoolInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button prliverpool;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liverpool_info);
+        prliverpool = findViewById(R.id.prliverpool);
+        prliverpool.setOnClickListener(this);
 
 
     }
 
     public void onClick(View v) {
 
+
+        if(v==prliverpool){
         Intent i = new Intent(this, LiverpoolRosterActivity.class);
-        startActivity(i);
+        startActivity(i);}
     }
 }
